48062c47fb8399d29196fdee73eacc569a4797f0
freetimezp <kraftukrnet@ukr.net>
2023-01-11 21:17:58 +0200

    05. js-portfolio-08-photo. Show, hide menu.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,9 +19,44 @@ window.addEventListener("load", function() {
 });
 
 
+// mobile nav toggle
 
+const [navTogglers, navLinks, navbar, overlay] = [
+    document.querySelectorAll("[data-nav-toggler]"),
+    document.querySelectorAll("[data-nav-link]"),
+    document.querySelector("[data-navbar]"),
+    document.querySelector("[data-overlay]")
+];
 
+const toggleNav = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+};
 
+addEventOnElements(navTogglers, "click", toggleNav);
+
+const closeNav = function() {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.classList.remove("active");
+};
+
+addEventOnElements(navLinks, "click", closeNav);
+
+// header
+
+const header = document.querySelector("[data-header]");
+
+const activeElementOnScroll = function() {
+    if(window.scrollY > 50) {
+        header.classList.add("active");
+    }else{
+        header.classList.remove("active");
+    }
+};
+
+window.addEventListener("scroll", activeElementOnScroll);
 
 
 
