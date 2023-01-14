240d526ad3d76ac00c68078660280933f7c983b2
freetimezp <kraftukrnet@ukr.net>
2023-01-14 22:57:32 +0200

    14. js-portfolio-08-photo. Animation reveal.

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,6 +141,24 @@ window.addEventListener("scroll", function() {
 });
 
 
+// scroll reveal
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+    for(let i = 0; i < revealElements.length; i++) {
+        const elementIsInScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15;
+
+        if(elementIsInScreen) {
+            revealElements[i].classList.add("revealed");
+        }else{
+            revealElements[i].classList.remove("revealed");
+        }
+    }
+};
+
+window.addEventListener("scroll", scrollReveal);
+scrollReveal();
 
 
 
